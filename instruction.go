7a7794284d261a7902897a0d30d4f96b957af4cd
Kier Davis <kierdavis@gmail.com>
2015-03-09 21:09:09 +0000

    Improve instruction stringification

--- instruction.go
+++ instruction.go
@@ -67,70 +67,80 @@ const (
     NumInstructions
 )
 
-func (inst Instruction) String() string {
-    switch inst {
-    case ADC:       return "ADC"
-    case ADD:       return "ADD"
-    case ADIW:      return "ADIW"
-    case AND:       return "AND"
-    case ANDI:      return "ANDI"
-    case ASR:       return "ASR"
-    case BCLR:      return "BCLR"
-    case BLD:       return "BLD"
-    case BRBC:      return "BRBC"
-    case BRBS:      return "BRBS"
-    case BREAK:     return "BREAK"
-    case BSET:      return "BSET"
-    case BST:       return "BST"
-    case CALL:      return "CALL"
-    case CBI:       return "CBI"
-    case COM:       return "COM"
-    case CP:        return "CP"
-    case CPC:       return "CPC"
-    case CPI:       return "CPI"
-    case CPSE:      return "CPSE"
-    case DEC:       return "DEC"
-    case DES:       return "DES"
-    case EICALL:    return "EICALL"
-    case EIJMP:     return "EIJMP"
-    case ELPM_R0:   return "ELPM_R0"
-    case ELPM:      return "ELPM"
-    case ELPM_INC:  return "ELPM_INC"
-    case EOR:       return "EOR"
-    case FMUL:      return "FMUL"
-    case FMULS:     return "FMULS"
-    case FMULSU:    return "FMULSU"
-    case ICALL:     return "ICALL"
-    case IJMP:      return "IJMP"
-    case IN:        return "IN"
-    case INC:       return "INC"
-    case JMP:       return "JMP"
-    case LAC:       return "LAC"
-    case LAS:       return "LAS"
-    case LAT:       return "LAT"
-    case LD_X:      return "LD_X"
-    case LD_X_INC:  return "LD_X_INC"
-    case LD_X_DEC:  return "LD_X_DEC"
-    case LD_Y_INC:  return "LD_Y_INC"
-    case LD_Y_DEC:  return "LD_Y_DEC"
-    case LDD_Y:     return "LDD_Y"
-    case LD_Z_INC:  return "LD_Z_INC"
-    case LD_Z_DEC:  return "LD_Z_DEC"
-    case LDD_Z:     return "LDD_Z"
-    case LDI:       return "LDI"
-    case LDS:       return "LDS"
-    case LDS_SHORT: return "LDS_SHORT"
-    case LPM_R0:    return "LPM_R0"
-    case LPM:       return "LPM"
-    case LPM_INC:   return "LPM_INC"
-    case LSR:       return "LSR"
-    case MOV:       return "MOV"
-    case MOVW:      return "MOVW"
-    case MUL:       return "MUL"
-    case MULS:      return "MULS"
-    case MULSU:     return "MULSU"
-    default:        return ""
+var instStrings = [...]string{
+    "ADC",
+    "ADD",
+    "ADIW",
+    "AND",
+    "ANDI",
+    "ASR",
+    "BCLR",
+    "BLD",
+    "BRBC",
+    "BRBS",
+    "BREAK",
+    "BSET",
+    "BST",
+    "CALL",
+    "CBI",
+    "COM",
+    "CP",
+    "CPC",
+    "CPI",
+    "CPSE",
+    "DEC",
+    "DES",
+    "EICALL",
+    "EIJMP",
+    "ELPM_R0",
+    "ELPM",
+    "ELPM_INC",
+    "EOR",
+    "FMUL",
+    "FMULS",
+    "FMULSU",
+    "ICALL",
+    "IJMP",
+    "IN",
+    "INC",
+    "JMP",
+    "LAC",
+    "LAS",
+    "LAT",
+    "LD_X",
+    "LD_X_INC",
+    "LD_X_DEC",
+    "LD_Y_INC",
+    "LD_Y_DEC",
+    "LDD_Y",
+    "LD_Z_INC",
+    "LD_Z_DEC",
+    "LDD_Z",
+    "LDI",
+    "LDS",
+    "LDS_SHORT",
+    "LPM_R0",
+    "LPM",
+    "LPM_INC",
+    "LSR",
+    "MOV",
+    "MOVW",
+    "MUL",
+    "MULS",
+    "MULSU",
+}
+
+func init() {
+    if len(instStrings) != int(NumInstructions) {
+        panic("package avr: len(instStrings) != NumInstructions")
     }
+}
+
+func (inst Instruction) String() string {
+    if inst >= 0 && inst < NumInstructions {
+        return instStrings[inst]
+    }
+    return "<invalid>"
 }
 
 func (inst Instruction) IsTwoWord() bool {
