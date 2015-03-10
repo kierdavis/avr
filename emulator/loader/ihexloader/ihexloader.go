9dcf6e6e92e86c341be3d97d7aa9caef70305780
Kier Davis <kierdavis@gmail.com>
2015-03-10 19:40:16 +0000

    Add IHEX program file loader

---
+++ emulator/loader/ihexloader/ihexloader.go
@@ -0,0 +1,31 @@
+// Package ihexloader encapsulates loading of IHEX files into an Emulator.
+package ihexloader
+
+import (
+    "github.com/kierdavis/avr/emulator"
+    "github.com/kierdavis/ihex-go"
+    "io"
+)
+
+// LoadIHEX parses an IHEX file from r and loads the program data contained in
+// it into em.
+func LoadIHEX(em *emulator.Emulator, r io.Reader) (err error) {
+    dec := ihex.NewDecoder(r)
+    buf := make([]uint16, 0, 8)
+    
+    for dec.Scan() {
+        rec := dec.Record()
+        if rec.Type == ihex.Data {
+            buf = buf[:0]
+            for i := 0; i+1 < len(rec.Data); i += 2 {
+                lo := uint16(rec.Data[i])
+                hi := uint16(rec.Data[i+1])
+                buf = append(buf, (hi << 8) | lo)
+            }
+            
+            em.WriteProg(rec.Address, buf)
+        }
+    }
+    
+    return dec.Err()
+}
