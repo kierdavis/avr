8c6b55a77ccc42598b8f1ddb9aaeb419adf39ed1
Kier Davis <kierdavis@gmail.com>
2015-03-10 20:00:27 +0000

    Write initial version of frontend (cmd/avrem)

---
+++ cmd/avrem/avrem.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "github.com/kierdavis/avr/emulator"
+    "github.com/kierdavis/avr/loader/ihexloader"
+    "github.com/kierdavis/avr/spec"
+    "os"
+)
+
+func main() {
+    flag.Parse()
+    
+    if flag.NArg() < 1 {
+        fmt.Fprintf(os.Stderr, "usage: %s <program.hex>\n", os.Args[0])
+        os.Exit(2)
+    }
+    
+    runEmulator()
+}
+
+func runEmulator() {
+    em := emulator.NewEmulator(spec.ATmega168)
+    loadProgram(em)
+    
+    em.Run(1000)
+    
+    fmt.Println("OK.")
+}
+
+func loadProgram(em *emulator.Emulator) {
+    f, err := os.Open(flag.Arg(0))
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+        os.Exit(1)
+    }
+    
+    err = ihexloader.Load(em, f)
+    f.Close()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %s\n", err.Error())
+        os.Exit(1)
+    }
+}
